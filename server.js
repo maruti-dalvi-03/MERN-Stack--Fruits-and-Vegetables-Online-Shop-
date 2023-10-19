e9f1df8e252615ab4d06d1ad8db5d9f4ad370302
maruti-dalvi-03 <148435084+maruti-dalvi-03@users.noreply.github.com>
2023-10-19 14:45:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ import categoryRoutes from './routes/categoryRoute.js'
 import productRoutes from './routes/productRoute.js'; 
 import cors from 'cors';
 import path from 'path';
-import {fileURLTOPath} from 'url';
+import { fileURLTOPath } from 'url';
 
 //configure env
 dotenv.config();
